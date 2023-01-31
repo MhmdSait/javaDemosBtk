60a89f1933b8a58f01b189df99b5acbc7add623e
MhmdSait <mehmedsaidtasdemir@gmail.com>
2023-01-31 22:42:15 +0300

    7.ders

---
+++ src/ders07_miniProjeler/C03_MukemmelSayi.java
@@ -0,0 +1,11 @@
+package ders07_miniProjeler;
+
+public class C03_MukemmelSayi {
+    public static void main (String[] args){
+        //6 -->1,2,3
+        //28-->1,2,4,7,14
+        int number = 5;
+
+
+    }
+}
