511b8e0c3b3291e47ff06d625247fc728a07fef7
MhmdSait <mehmedsaidtasdemir@gmail.com>
2023-01-31 23:01:44 +0300

    fourth commit

--- src/ders07_miniProjeler/C03_MukemmelSayi.java
+++ src/ders07_miniProjeler/C03_MukemmelSayi.java
@@ -4,7 +4,20 @@ public class C03_MukemmelSayi {
     public static void main (String[] args){
         //6 -->1,2,3
         //28-->1,2,4,7,14
-        int number = 5;
+        int number = 8128;
+        int total = 0;
+
+        for (int i=1; i<number; i++){
+            if(number % i ==0){
+                total = total +i;
+            }
+        }
+
+        if(total == number){
+            System.out.println("Mükemmel sayıdır");
+        }else {
+            System.out.println("Mükemmel sayı degildir");
+        }
 
 
     }
